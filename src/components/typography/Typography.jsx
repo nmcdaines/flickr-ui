5befeb57350025e0e2a7de2c45d78cc125c164fd
Nate Daines <nate@thedaines.org>
2021-02-21 11:28:39 +1100

    created a title typography component

---
+++ src/components/typography/Typography.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import styled from 'styled-components';
+
+export const Title = styled.h3`
+  color: rgba(0,0,0,0.7);
+  font-weight: 400;
+  font-size: 22px;
+  margin: 0px 0px 10px 0px;
+`;
