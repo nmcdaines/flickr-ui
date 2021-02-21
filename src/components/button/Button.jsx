f75f384ab1b0543669ea829ca6fef511156d6ce3
Nate Daines <nate@thedaines.org>
2021-02-21 11:28:54 +1100

    created a simple button

---
+++ src/components/button/Button.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import styled from 'styled-components';
+
+export const Button = styled.a`
+  display: inline-block;
+  color: #2196F3;
+  
+  padding: 10px;
+  border-radius: 4px;
+  cursor: pointer;
+
+  &:hover {
+    background: rgba(33,150,243,0.2);
+  }
+`;
