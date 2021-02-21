3dcadcc798a381108c5baf27945ddee454cef307
Nate Daines <nate@thedaines.org>
2021-02-21 11:29:49 +1100

    created a provider that manages application state

---
+++ src/core/FlickrContext.jsx
@@ -0,0 +1,73 @@
+import React, { useState, useEffect } from 'react';
+
+const FlickrContext = React.createContext(undefined);
+const TagsContext = React.createContext(undefined);
+const SetTagsContext = React.createContext(undefined);
+const LoadingContext = React.createContext(true);
+
+export const FlickrProvider = ({ children }) => {
+  const [flickrData, setFlickrData] = useState({});
+  const [tags, setTags] = useState(['nature', 'birds']);
+  const [loading, setLoading] = useState(true);
+
+  const setTagsFn = (changedTags = []) => {
+    setLoading(true);
+    setTags(changedTags);
+  }
+
+  useEffect(() => {
+    window.jsonFlickrFeed = (value) => {
+      console.log('here', value);
+
+      if (!value) { return; }
+      setLoading(false);
+      setFlickrData(value?.items);
+    };
+
+    return () => {
+      // remove function 'jsonFlickrFeed' from window on component unmount
+      window.jsonFlickrFeed = undefined;
+    }
+  }, []);
+
+  console.log(flickrData);
+
+  return (
+    <>
+      <FlickrContext.Provider value={flickrData}>
+        <TagsContext.Provider value={tags}>
+          <SetTagsContext.Provider value={setTagsFn}>
+            <LoadingContext.Provider value={loading}>
+              { children }
+            </LoadingContext.Provider>
+          </SetTagsContext.Provider>
+        </TagsContext.Provider>
+      </FlickrContext.Provider>
+    </>
+  );
+}
+
+export const useFlickr = () => {
+  const context = React.useContext(FlickrContext);
+  if (context === undefined) {
+    throw new Error('useFlickr must be used within a FlickrProvider');
+  }
+  return context;
+}
+
+export const useTags = () => {
+  const tagsContext = React.useContext(TagsContext);
+  const setTagsContext = React.useContext(SetTagsContext);
+  if (tagsContext === undefined || setTagsContext === undefined) {
+    throw new Error('useTags must be used within a FlickrProvider');
+  }
+  return [tagsContext, setTagsContext];
+}
+
+export const useLoading = () => {
+  const context = React.useContext(LoadingContext);
+  if (context === undefined) {
+    throw new Error('useLoading must be used within a FlickrProvider');
+  }
+  return context;
+}
